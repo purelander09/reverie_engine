980efcd0123b15e1b885d14413f4f4595dfdba4e
ryanboone <ryan.a.boone@gmail.com>
2020-08-27 15:30:50 -0500

    Working on adding and fixing parsed message fields

--- lib/reverie_engine_web/controllers/message_history_controller.ex
+++ lib/reverie_engine_web/controllers/message_history_controller.ex
@@ -6,10 +6,25 @@ defmodule ReverieEngineWeb.MessageHistoryController do
   alias ReverieEngine.Receiver
   alias ReverieMessageToolkit.HL7GenericParser
 
+  @spec show(Plug.Conn.t(), map) :: Plug.Conn.t()
   def show(conn, %{"id" => id}) do
     message = Receiver.get_message!(id)
     parsed_message = HL7GenericParser.parse(message.message_body)
+
+    msh = Map.get(parsed_message, :MSH)
+    version = Map.get(parsed_message, :version)
+    field_separator = Map.get(parsed_message, :field_separator)
+    repeat_separator = Map.get(parsed_message, :repeat_separator)
+
     IEx.pry()
-    render(conn, "show.html", message: message, parsed_message: parsed_message)
+    render(
+      conn,
+      "show.html",
+      message: message,
+      msh: msh,
+      version: version,
+      field_separator: field_separator,
+      repeat_separator: repeat_separator
+    )
   end
 end
